3555a45ace50fdc94362a8c49ffd13dba077f7d4
Yanick Nedderhoff <yanicknedderhoff@gmail.com>
2019-05-31 16:59:22 +0200

    add laptop setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Docker 
+# https://docs.docker.com/install/linux/docker-ce/debian/#install-docker-ce-1
+sudo apt-get install docker-ce docker-ce-cli containerd.io
